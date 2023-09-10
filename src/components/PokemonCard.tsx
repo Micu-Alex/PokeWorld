4cd724a653bafc65d077f259a4c9160ec6843f09
Micu-Alex <alexmicu389@gmail.com>
2023-09-10 21:20:19 +0300

    Added dinamic  backround to card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -21,10 +21,36 @@ const PokemonCard = ({ pokemon }: Props) => {
     color: "inherit",
   };
 
+  const typeColors: { [key: string]: string } = {
+    normal: " #C2C2A1",
+    flying: " #BAB0D5",
+    ghost: " #735797",
+    dark: " #333",
+    steel: " #CCCCDE,",
+    poison: " #7C538C",
+    ground: " #B1736C",
+    grass: " #48D0B0",
+    fire: " #FB6C6C",
+    electric: "#FFD86F;",
+    fairy: "#f469a9;",
+    bug: "#C3CE75;",
+    fighting: " #d6b591",
+    water: "#609FB5",
+    psychic: "#9B7FA6",
+    ice: "#7FCCEC",
+    rock: " #a6aab6",
+    dragon: "#F9BE00",
+  };
+
+  const backgroundColor = typeColors[pokemon.types[0].type.name];
+
   return (
     <Card
       sx={{
+        borderRadius: 7,
         maxWidth: 300,
+        backgroundColor: backgroundColor,
+        boxShadow: `0px 4px 6px ${backgroundColor}`,
         transition: "transform 0.2s ease",
         "&:hover": {
           transform: "scale(1.05)",
