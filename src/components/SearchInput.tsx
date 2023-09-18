f877de5cf787de09236dac00c0a9a8cd425c0728
Micu-Alex <alexmicu389@gmail.com>
2023-09-18 23:23:32 +0300

    Reformed search input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,31 +1,20 @@
-import { InputAdornment, TextField } from "@mui/material";
+import { IconButton, InputBase } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchInput = () => {
   return (
-    <TextField
-      id="outlined-basic"
-      variant="outlined"
+    <InputBase
+      placeholder="Search..."
       fullWidth
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon sx={{ color: "#211001" }} />
-          </InputAdornment>
-        ),
-      }}
+      startAdornment={
+        <IconButton>
+          <SearchIcon sx={{ color: "#211001" }} />
+        </IconButton>
+      }
       sx={{
-        "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-          borderWidth: "2px",
-          borderRadius: 5,
-          borderColor: "#211001",
-        },
-        "& .MuiInputLabel-root": {
-          fontWeight: "bold",
-          borderWidth: "2px",
-          borderRadius: 5,
-          borderColor: "#211001",
-        },
+        fontWeight: "bold",
+        border: "2px solid #211001",
+        borderRadius: 7,
       }}
     />
   );
