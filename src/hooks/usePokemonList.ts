77a60f5dd2f99e9bfefb4996d405fed641027b10
Micu-Alex <alexmicu389@gmail.com>
2023-09-18 13:34:30 +0300

    fixed indentation issue

--- src/hooks/usePokemonList.ts
+++ src/hooks/usePokemonList.ts
@@ -3,15 +3,12 @@ import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 
 
-
  export interface  PokemonsList {
     name: string
     url: string
   }
 
-
 const apiClient= new APIClient<PokemonsList>("/pokemon?limit=151")
-
 
   const usePokemonsList = () => 
   useQuery({
