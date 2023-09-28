60ee1ebb98493a0e5587dbd32cc8cd3d201ad159
Micu-Alex <alexmicu389@gmail.com>
2023-09-28 21:03:26 +0300

    Refactor Pokemon interface

--- src/entities/Pokemon.ts
+++ src/entities/Pokemon.ts
@@ -1,30 +1,36 @@
 export default interface Pokemon {
-    id: number;
-    name: string;
-    height: number;
-    weight: number;
-    sprites: {
-      other: {
-        dream_world: {
-          front_default: string;
-        };
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  sprites: {
+    other: {
+      dreamWorld: {
+        frontDefault: string;
       };
     };
-    stats: Array<{
-      base_stat: number;
-      stat: {
-        name: string;
-      };
-    }>;
-    types: Array<{
-      type: {
-        name: string
-      }
-    }>;
-    abilities: Array<{
-      ability: {
-        name: string
-        url: string
-      }
-    }>
-  }
+  };
+  stats: Stat[];
+  types: PokemonType[];
+  abilities: Ability[];
+}
+
+interface Stat {
+  baseStat: number;
+  stat: {
+    name: string;
+  };
+}
+
+interface PokemonType {
+  type: {
+    name: string;
+  };
+}
+
+interface Ability {
+  ability: {
+    name: string;
+    url: string;
+  };
+}
